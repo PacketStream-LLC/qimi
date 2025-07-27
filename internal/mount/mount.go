82306fe275b5010050e3a68e6796039e345b36b6
Alex4386 <psh010209@gmail.com>
2025-07-27 21:02:34 +0900

    chore: wow

--- internal/mount/mount.go
+++ internal/mount/mount.go
@@ -6,9 +6,11 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"errors"
 
 	qimiexec "github.com/packetstream-llc/qimi/internal/exec"
 	"github.com/packetstream-llc/qimi/internal/nbd"
+ 	"github.com/packetstream-llc/qimi/internal/logger"
 )
 
 type Mounter struct {
@@ -45,6 +47,7 @@ func (m *Mounter) Mount(imagePath string, readOnly bool) (string, error) {
 }
 
 func (m *Mounter) MountWithPartition(imagePath string, readOnly bool, partitionNum int) (string, error) {
+	logger.Debug("mounting image: %s, readOnly: %t, partitionNum: %d", imagePath, readOnly, partitionNum)
 	absPath, err := filepath.Abs(imagePath)
 	if err != nil {
 		return "", fmt.Errorf("failed to get absolute path: %w", err)
@@ -54,11 +57,14 @@ func (m *Mounter) MountWithPartition(imagePath string, readOnly bool, partitionN
 		return "", fmt.Errorf("image file not found: %w", err)
 	}
 
+	logger.Debug("absolute path of image: %s", absPath)
+	logger.Debug("creating mount point in: %s", m.mountDir)
 	mountPoint := filepath.Join(m.mountDir, filepath.Base(absPath)+".mount")
 	if err := os.MkdirAll(mountPoint, 0755); err != nil {
 		return "", fmt.Errorf("failed to create mount point: %w", err)
 	}
 
+	logger.Debug("mount point created: %s", mountPoint)
 	if err := m.mountQemuImage(absPath, mountPoint, readOnly, partitionNum); err != nil {
 		os.RemoveAll(mountPoint)
 		return "", err
@@ -68,6 +74,8 @@ func (m *Mounter) MountWithPartition(imagePath string, readOnly bool, partitionN
 }
 
 func (m *Mounter) Unmount(mountPoint string) error {
+	logger.Debug("unmounting mount point: %s", mountPoint)
+
 	// Try to unmount, but don't fail if already unmounted
 	cmd := exec.Command("umount", mountPoint)
 	cmd.Run() // Ignore error as it might already be unmounted
@@ -84,36 +92,45 @@ func (m *Mounter) Unmount(mountPoint string) error {
 }
 
 func (m *Mounter) mountQemuImage(imagePath, mountPoint string, readOnly bool, partitionNum int) error {
+	logger.Debug("mounting QEMU image: %s to %s, readOnly: %t, partitionNum: %d", imagePath, mountPoint, readOnly, partitionNum)
 	nbdDevice, err := nbd.FindFreeNBDDevice()
 	if err != nil {
 		return err
 	}
 
+	logger.Debug("Found free NBD Device! Using NBD device: %s", nbdDevice)
+	logger.Debug("Connecting image %s to NBD device %s", imagePath, nbdDevice)
 	if err := nbd.ConnectImage(imagePath, nbdDevice, readOnly); err != nil {
+		m.doDiscount(nbdDevice)
 		return err
 	}
 
+	logger.Debug("Probing partitions on NBD device %s", nbdDevice)
 	if err := nbd.ProbePartitions(nbdDevice); err != nil {
-		nbd.DisconnectDevice(nbdDevice)
+		m.doDiscount(nbdDevice)
 		return err
 	}
 
+	logger.Debug("Getting partition device for partition number %d on NBD device %s", partitionNum, nbdDevice)
 	partition, err := nbd.GetPartitionDevice(nbdDevice, partitionNum)
 	if err != nil {
-		nbd.DisconnectDevice(nbdDevice)
+		m.doDiscount(nbdDevice)
 		return err
 	}
 
 	// Build mount options
+	logger.Debug("Mounting partition %s to mount point %s", partition, mountPoint)
 	mountOpts := []string{}
 	if readOnly {
+		logger.Debug("Mounting in read-only mode")
 		mountOpts = append(mountOpts, "-r")
 	}
 	mountOpts = append(mountOpts, partition, mountPoint)
 
+	logger.Debug("Executing mount command: %s", strings.Join(mountOpts, " "))
 	cmd := exec.Command("mount", mountOpts...)
 	if output, err := cmd.CombinedOutput(); err != nil {
-		nbd.DisconnectDevice(nbdDevice)
+		m.doDiscount(nbdDevice)
 		return fmt.Errorf("failed to mount %s to %s: %w\nOutput: %s", partition, mountPoint, err, string(output))
 	}
 
@@ -121,17 +138,26 @@ func (m *Mounter) mountQemuImage(imagePath, mountPoint string, readOnly bool, pa
 	nbdFile := filepath.Join(m.metadataDir, filepath.Base(mountPoint)+".nbd")
 	if err := os.WriteFile(nbdFile, []byte(nbdDevice), 0644); err != nil {
 		exec.Command("umount", mountPoint).Run()
-		nbd.DisconnectDevice(nbdDevice)
+		m.doDiscount(nbdDevice)
 		return fmt.Errorf("failed to save nbd info: %w", err)
 	}
 
 	return nil
 }
 
+func (m *Mounter) doDiscount(nbdDevice string) {
+}
+
 func (m *Mounter) disconnectNBD(mountPoint string) error {
 	nbdFile := filepath.Join(m.metadataDir, filepath.Base(mountPoint)+".nbd")
 	data, err := os.ReadFile(nbdFile)
 	if err != nil {
+		if os.IsNotExist(err) {
+			// The NBD doesn't exist. let's check lsblk.
+			logger.Warn("NBD metadata file not found, please run lsblk for NBD devices")
+			return errors.New("NBD metadata file mismatch")
+		}
+
 		return nil
 	}
 
